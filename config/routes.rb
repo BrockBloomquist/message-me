7c5f93bb5ab2dbfd443ee48514d757d931016f50
BrockBloomquist <brockbloomquist1@gmail.com>
2024-05-09 16:02:52 -0500

    Setup sessions for logging in and logging out, need to setup confirmation messages partial and conditional rendering

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "chatroom#index"
   get "login", to: "sessions#new"
+  post 'login', to: "sessions#create"
   get "signup", to: "users#new"
+  delete "logout", to: 'sessions#destroy'
 end
