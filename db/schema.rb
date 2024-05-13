afd13b0b4f225bd5742f2a883618e021c1a2ac28
BrockBloomquist <brockbloomquist1@gmail.com>
2024-05-12 22:38:34 -0500

    Created a migration for users to check if they're an admin and added validations to the user and message models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_034943) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_031542) do
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_034943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.boolean "admin", default: false
   end
 
 end
